9e0fbc979d43bfd24e19ae049cc325a326d8fad9
root <root@localhost.localdomain>
2019-05-27 20:31:52 -0400

    does this work

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      phrase1 = /^\/uod$/;
+      phrase1 = /^\/test$/;
 
 
   if(request.text && phrase1.test(request.text)) {
@@ -25,22 +25,8 @@ function postMessage(num) {
   var botResponse, options, body, botReq;
 
     if(num == 1){
-      // uod
-      return new Promise(function (resolve, reject){
-        request('https://vtcc-ros.github.io/POD-HTML.htm', (error, response, html) => {
-    if (!error && response.statusCode == 200){
-      const body = cheerio.load(html).text();
-      var uod = parseuod(body);
-      var date = parsedate(body);
-      var finalstring = "The Uniform of the Day for " + date + " is " + uod;
-      //conv.close(finalstring);
-      console.log(finalstring);
-      botResponse = finalstring;
-      resolve();
-    }
-  });
-  });
-    //botResponse = "Ciaraan is the BIG gay.";
+
+    botResponse = "yeet";
   }
 
 
@@ -72,33 +58,6 @@ function postMessage(num) {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
-}
-
-function parseuod(uod) {
-  var uodstring1 = uod.split('Uniform of the Day:')[1];
-  var uodstring2 = uodstring1.split("<o")[0];
-  return uodstring2;
-}
-
-function parsedate(date) {
-  var month = ['January', 'February', 'March', 'April', 'May', 'August', 'September', 'October', 'November', 'Decemeber'];
-  var year = [2019, 2020, 2021, 2022, 2023, 2024, 2025];
-  var dateparse1 = date.split('PLAN OF THE DAY')[1];
-  var dateparse2 = dateparse1.split('VIRGINIA TECH')[0];
-  var finalstring = "";
-  for (i = 0; i < month.length; i++){
-    for (j = 0; j < year.length; j++){
-    if (dateparse2.indexOf(month[i]) >= 1){
-      if (dateparse2.indexOf(year[j]) >= 1){
-  	  var index = dateparse2.indexOf(month[i]);
-      var start = index - 3;
-      var end = dateparse2.indexOf(year[j]) + 4;
-      finalstring = dateparse2.substring(start, end);
-      }
-      }
-    }
-  }
-  return finalstring;
 }
 
 exports.respond = respond;
